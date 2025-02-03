4eaa883d72fc8c2b9af4e12766c7a1237ec16e36
Rajin Gangadharan <rajin.gangadharan@remitbee.com>
2025-02-03 10:14:50 +0530

    Initial Commit

---
+++ NewsTests/NewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsTests.swift
+//  NewsTests
+//
+//  Created by Rajin Gangadharan on 03/02/25.
+//
+
+import Testing
+@testable import News
+
+struct NewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
